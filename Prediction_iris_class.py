72e4eb4e807761ee370342bba434f109ffe00d91
vishal-kr-yadav <vkyadav9748@gmail.com>
2017-12-12 22:16:55 +0530

    prediction of iris class implement

--- Prediction_iris_class.py
+++ Prediction_iris_class.py
@@ -18,38 +18,24 @@ from sklearn.svm import SVC
 url="/home/vishal/Documents/iris.data";
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 
-dataset=pandas.read_csv(url,names=names);
+dataset=pandas.read_csv(url,names=names)
 # Split-out validation dataset
 array = dataset.values
-X = array[:,0:4]
-Y = array[:,4]
+X = array[:,0:4] #all value is stored
+Y = array[:,4] #class name is stored
 validation_size = 0.20
 seed = 7
+#sklearn.model_selection.train_test_split(*arrays, **options)[source]
+#If float, should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 scoring = 'accuracy'
+# X_train and Y_train for preparing models and a X_validation and Y_validation sets that we can use later.
+#print(Y_validation)
 # Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
-# evaluate each model in turn
-results = []
-names = []
-for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
 
 # # Make predictions on validation dataset
-# knn = KNeighborsClassifier()
-# knn.fit(X_train, Y_train)
-# predictions = knn.predict(X_validation)
-# print(accuracy_score(Y_validation, predictions))
-# print(confusion_matrix(Y_validation, predictions))
-# print(classification_report(Y_validation, predictions))
+lr = LogisticRegression()
+lr.fit(X_train, Y_train)
+T_validation=[[7.0,3.2,4.7,1.4,]]
+predictions = lr.predict(T_validation)
+print(predictions)
